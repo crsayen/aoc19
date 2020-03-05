b0cda20f3e0e22555bd57f6c85f2184b1dd76246
Chris Sayen <chrissayen@iMac.local>
2020-03-04 22:17:07 -0500

    a bit stuck. op 3 doesn't seem to do anything

--- advent5.2.py
+++ advent5.2.py
@@ -1,6 +1,5 @@
 import sys
 from instructions5_1 import mem
-
 ID = 5
 
 i = e = o = m = p = d = 0
@@ -13,19 +12,28 @@ modeselect = lambda m: {
 nparams = [3,3,3,1,1,2,2,3,3] + [0] * 99
 
 def jump_true(a):
+    global i
+    print("jumped!")
     if a != 0:
-        print(a)
-        print(modeselect(m[0])(p[0]) - 1)
-        i = modeselect(m[0])(p[0]) - 1
+        i = modeselect(m[-1])(d) - i
+        return "jump"
 
 def jump_false(a):
+    global i
+    print("jumped!")
     if a == 0:
-        i = modeselect(m[0])(p[0]) - 1
+        i = modeselect(m[-1])(d) - i
+        return "jump"
+
+def inp():
+    global mem
+    mem[255] = 1114
+    return None
 
 opselect = lambda op: {
     1: lambda a, b: a + b,
     2: lambda a, b: a * b,
-    3: lambda: 5,
+    3: inp,
     4: "print",
     5: jump_true,
     6: jump_false,
@@ -35,21 +43,24 @@ opselect = lambda op: {
 }.get(op)
 
 def do(ptr):
-    print(mem[ptr: ptr + 10])
+    global i,m,d
+    m=d=0
+    #print(mem[ptr: ptr + 10])
     e = str(mem[ptr])
     o, m = int(''.join(e[-2:])), [ int(c) for c in e[-3::-1] ]
     m = ((m + [0] * (nparams[o] - len(m))))
     p,d = mem[ptr + 1: ptr + nparams[o]],  mem[ptr + nparams[o]]
-    print(f"\n{i=}\n{e=}\n{o=}\n{m=}\n{p=}\n{d=}\n")
-    f = opselect(0)
-    if f is not None:
+    #print(f"\n{i=}\n{e=}\n{o=}\n{m=}\n{p=}\n{d=}\n")
+    if (f := opselect(o)) is not None:
         if f == "print":
-            print(mem[mem[i + 1]])
+            print(modeselect(m[-1])(d))
         elif f == "halt":
-            print(mem[0])
+            #print(mem[0])
             sys.exit()
         else:
-            mem[d] = f(*[ modeselect(m)(p) for (m,p) in zip(m, p) ])
+            if (r := f(*[ modeselect(_m)(p) for (_m,p) in zip(m, p) ])) == "jump":
+                return i
+            mem[d] = r
     return nparams[o] + 1
    
 while True:
